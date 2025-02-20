4adb9cd11c815adb3e6d3aca18b8d463a1809e95
Nathaniel Driggs <nathanieldriggs@gmail.com>
2025-02-20 15:47:10 -0700

    adding fold to existing templates

---
+++ templates/fold_and_cut_nanoGPT/run_experiments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=03:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=32768M   # memory per CPU core
+#SBATCH -J "fold_nanogpt"   # job name
+#SBATCH --output=logs/%A_output.out
+#SBATCH --error=logs/%A_errors.err
+
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load texlive
+python experiment.py --out_dir run_0
+python plot.py
