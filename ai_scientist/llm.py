cea2f844cad4521222c33040573f4a4ed0f2e69c
Shawn Huang <seanzy0716@gmail.com>
2025-02-24 10:43:25 -0700

    added Gemini model codes

--- ai_scientist/llm.py
+++ ai_scientist/llm.py
@@ -42,6 +42,11 @@ AVAILABLE_LLMS = [
     # Google Gemini models
     "gemini-1.5-flash",
     "gemini-1.5-pro",
+    "gemini-2.0-flash",
+    "gemini-2.0-flash-lite-preview-02-05",
+    "gemini-2.0-pro-exp-02-05",
+    "gemini-2.0-flash-thinking-exp-01-21",
+
 ]
 
 
